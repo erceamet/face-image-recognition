6961f2b63f338a8c9b7b1bfd8daf6c69e0321c15
Erce Amet <33298952+erceamet@users.noreply.github.com>
2020-06-11 00:46:10 +0100

    up

--- face-image-recognition/src/components/FaceRecognition/FaceRecognition.js
+++ face-image-recognition/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,33 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={imageUrl} alt="" width="500px" height="auto" />
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt=""
+          width="500px"
+          height="auto"
+        />
+        {boxes.map((box, i) => {
+          const { leftCol, topRow, rightCol, bottomRow } = box;
+          return (
+            <div
+              key={i}
+              id="face"
+              className="bounding-box"
+              style={{
+                top: topRow,
+                right: rightCol,
+                bottom: bottomRow,
+                left: leftCol,
+              }}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
